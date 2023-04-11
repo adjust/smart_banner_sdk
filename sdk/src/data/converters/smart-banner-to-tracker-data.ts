ae680d21f9b55c9d8a316869e1347258c4782f5b
Yara Matkova <yara.matkova@adjust.com>
2023-04-11 23:20:43 +0200

    feat: build tracker url from transformed BE data

---
+++ sdk/src/data/converters/smart-banner-to-tracker-data.ts
@@ -0,0 +1,19 @@
+import { SmartBannerData } from '../types';
+import { TrackerData } from '../../domain/tracker-builder';
+
+/**
+ * Converts SmartBannerData to TrackerData
+ * 
+ * @returns A new object containing only tracker template and localized Context
+ */
+export function convertSmartBannerToTracker(data: SmartBannerData, domain: string, locale?: string | null): TrackerData {
+  const { trackerUrl: { template, context }, localizations } = data;
+
+  const localization = locale && localizations ? localizations[locale] : null;
+  const localeContext = localization ? localization.context : {};
+
+  return {
+    template,
+    context: { ...context, ...localeContext, domain }
+  }
+}
