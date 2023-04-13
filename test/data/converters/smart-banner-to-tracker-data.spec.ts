4f75cf3ae1a76ceecf0b741c24c677bcd9379ada
Yara Matkova <yara.matkova@adjust.com>
2023-04-13 09:52:48 +0200

    style: lint issues fix

--- test/data/converters/smart-banner-to-tracker-data.spec.ts
+++ test/data/converters/smart-banner-to-tracker-data.spec.ts
@@ -1,22 +1,22 @@
-import { SmartBannerData, Localization } from "@sdk/data/types";
-import { convertSmartBannerToTracker } from "@sdk/data/converters/smart-banner-to-tracker-data";
+import { SmartBannerData, Localization } from '@sdk/data/types';
+import { convertSmartBannerToTracker } from '@sdk/data/converters/smart-banner-to-tracker-data';
 
 describe('Convertation of SmartBannerData to TrackerData', () => {
 
   const trackerData = {
     template: 'https://whatever',
     context: {
-      domain: "app.adjust.com",
-      tracker: "qtzy19",
-      campaign: "banner test 2",
-      adgroup: "en",
+      domain: 'app.adjust.com',
+      tracker: 'qtzy19',
+      campaign: 'banner test 2',
+      adgroup: 'en',
       deeplink: null
     }
-  }
+  };
 
   const localizationContext = {
     adgroup: 'ru'
-  }
+  };
 
   const localizations = {
     ru: {
@@ -27,29 +27,29 @@ describe('Convertation of SmartBannerData to TrackerData', () => {
     } as Localization
   } as { [key: string]: Localization };
 
-  const bannerData = { trackerUrl: trackerData, localizations } as SmartBannerData
+  const bannerData = { trackerUrl: trackerData, localizations } as SmartBannerData;
 
   it('converts data with default locale', () => {
-    expect(convertSmartBannerToTracker(bannerData)).toEqual(trackerData)
-  })
+    expect(convertSmartBannerToTracker(bannerData)).toEqual(trackerData);
+  });
 
   it('converts data considering locale', () => {
     const expected = {
       template: trackerData.template,
       context: { ...trackerData.context, ...localizationContext }
-    }
+    };
 
-    expect(convertSmartBannerToTracker(bannerData, null, 'ru')).toEqual(expected)
-  })
+    expect(convertSmartBannerToTracker(bannerData, null, 'ru')).toEqual(expected);
+  });
 
   it('converts data and adds custom domain', () => {
-    const domain = 'my.nice.domain'
+    const domain = 'my.nice.domain';
 
     const expected = {
       template: trackerData.template,
       context: { ...trackerData.context, domain }
-    }
+    };
 
-    expect(convertSmartBannerToTracker(bannerData, 'my.nice.domain')).toEqual(expected)
-  })
-})
+    expect(convertSmartBannerToTracker(bannerData, 'my.nice.domain')).toEqual(expected);
+  });
+});
