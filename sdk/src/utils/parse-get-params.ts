c8f2d0756b159946a3e76807f81db14e8fa6622f
Yara Matkova <yara.matkova@adjust.com>
2023-04-13 09:51:49 +0200

    test: add tests for new utils

--- sdk/src/utils/parse-get-params.ts
+++ sdk/src/utils/parse-get-params.ts
@@ -1,17 +1,23 @@
 /**
- * Extracts GET parameters from passed url and splits them to  `key: value` pairs
+ * Extracts GET parameters from passed url and splits them to `key: value` pairs.
+ * If URL contains parameter without a value then this parameter considered as boolean and equals true.
  * @returns an object containing GET parameters
  */
-export function parseGetParams(pageUrl: string): Record<string, string> {
-  const startIndex = pageUrl.indexOf('?')
+export function parseGetParams(pageUrl: string): Record<string, string | true> {
+  const startIndex = pageUrl.indexOf('?');
   if (startIndex < 0) {
-    return {}
+    return {};
   }
 
-  const pairs = pageUrl.substring(startIndex + 1).split('&')
+  const pairs = pageUrl.substring(startIndex + 1).split('&');
 
   return pairs.reduce((acc, pair) => {
-    const [key, value] = pair.split('=')
-    return { ...acc, [key]: value }
-  }, {})
+    const [key, value] = pair.split('=');
+
+    if (!key) {
+      return acc;
+    }
+
+    return { ...acc, [key]: value || true };
+  }, {});
 }
