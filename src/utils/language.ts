28f4b3c1bfb9a9921eee5633733a96f33ae59ecd
Yara Matkova <yara.matkova@adjust.com>
2023-05-09 00:46:53 +0200

    test: detecting browser language

--- src/utils/language.ts
+++ src/utils/language.ts
@@ -2,7 +2,7 @@ export function getLanguage(): string {
   let languageTag = null;
 
   if (navigator) {
-    const language = (Array.isArray(navigator.languages) && navigator.languages.length > 0) ? navigator.languages[0] : navigator.language;
+    const language = (Array.isArray(navigator.languages) && navigator.languages.length > 0) ? navigator.languages[0] : navigator.language || '';
     languageTag = language.split('-')[0];
   }
 
---
+++ test/utils/language.spec.ts
@@ -0,0 +1,64 @@
+import { getLanguage } from './../../src/utils/language';
+
+describe('Detecting browser language', () => {
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  const testSet = /*
+    [
+      [],
+      'ru-RU',
+      'ru'
+    ],
+    [
+      undefined,
+      'ru-RU',
+      'ru'
+    ],
+    [
+      undefined,
+      '',
+      'en'
+    ],
+    [
+      undefined,
+      undefined,
+      'en'
+    ],
+  ];*/
+
+  test.each([
+    {languages: ['fr', 'en-US'], language: 'ru-RU', expected: 'fr'},
+    {languages: ['en-US', 'fr'], language: 'ru-RU', expected: 'en'},
+  ])('reads language tag from Navigator.languages array', ({ languages, language, expected }) => {
+    jest.spyOn(global.navigator, 'languages', 'get').mockReturnValue(languages);
+    jest.spyOn(global.navigator, 'language', 'get').mockReturnValue(language);
+
+    expect(getLanguage()).toBe(expected)
+  })
+
+  test.each([
+    {languages: [], language: 'fr', expected: 'fr'},
+    {languages: undefined, language: 'ru-RU', expected: 'ru'},
+  ])('reads language tag from Navigator.language', ({ languages, language, expected }) => {
+    jest.spyOn(global.navigator, 'languages', 'get').mockReturnValue(languages as any);
+    jest.spyOn(global.navigator, 'language', 'get').mockReturnValue(language);
+
+    expect(getLanguage()).toBe(expected)
+  })
+
+  test.each([
+    {languages: [], language: undefined},
+    {languages: undefined, language: ''},
+  ])('returns default language if unable to read Navigator.languages nor Navigator.language', ({ languages, language }) => {
+    jest.spyOn(global.navigator, 'languages', 'get').mockReturnValue(languages as any);
+    jest.spyOn(global.navigator, 'language', 'get').mockReturnValue(language as any);
+
+    expect(getLanguage()).toBe('en')
+  })
+
+  it('returns default language if unable to read Navigator.languages nor Navigator.language', () => {
+
+  })
+})
