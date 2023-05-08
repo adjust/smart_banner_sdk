2bf566e81ffb35b4b3804faac0489f6c1762ee76
Yara Matkova <yara.matkova@adjust.com>
2023-05-09 00:46:53 +0200

    fix: typo

--- sdk/src/view/types.ts
+++ sdk/src/view/types.ts
@@ -1,6 +1,6 @@
 import { SmartBannerData } from '../data/api';
 
 type KeyOfSmartBannerViewData = 'position' | 'size' | 'iconUrl' | 'title' | 'titleColor' | 'description' |
-  'descriptionColor' | 'buttonLabel' | 'buttonColor' | 'backgoundUrl' | 'backgoundColor';
+  'descriptionColor' | 'buttonLabel' | 'buttonColor' | 'backgroundUrl' | 'backgroundColor';
 
 export type SmartBannerViewData = Pick<SmartBannerData, KeyOfSmartBannerViewData>
