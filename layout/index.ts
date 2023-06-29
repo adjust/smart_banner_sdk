e04e4bb64000b7147e433b86c6e48089ad84957a
Yara Matkova <yara.matkova@adjust.com>
2023-06-29 14:02:30 +0200

    refactor: add factory to create layout

--- layout/index.ts
+++ layout/index.ts
@@ -1,9 +1,12 @@
-import { SmartBannerView } from './src/smart-banner-view';
+import { SmartBannerLayout, SmartBannerView } from './src/smart-banner-view';
+import { SmartBannerLayoutFactory } from './src/smart-banner-view-factory';
 import { SmartBannerViewData, Position, BannerSize } from './src/data-types';
 
 export {
   SmartBannerView,
   SmartBannerViewData,
   Position,
-  BannerSize
+  BannerSize,
+  SmartBannerLayout,
+  SmartBannerLayoutFactory
 };
