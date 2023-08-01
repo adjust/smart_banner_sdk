4bff3099da4e143f224965d5fec1ddfc991384d6
Yara Matkova <yara.matkova@adjust.com>
2023-08-01 11:24:28 +0200

    refac: move banner text to a class

---
+++ layout/src/text/index.ts
@@ -0,0 +1,48 @@
+import { SmartBannerViewData } from '../data-types';
+
+import styles from './styles.module.scss';
+
+export enum TextType {
+  Title,
+  Description
+}
+
+export class BannerText {
+  private element: HTMLElement;
+
+  constructor(type: TextType, private text?: string, private color?: string) {
+    if (type === TextType.Title) {
+      this.element = document.createElement('h4');
+    } else {
+      this.element = document.createElement('p');
+    }
+
+    this.element.className = styles['banner-text'];
+  }
+
+  private renderText(text?: string, color?: string) {
+    if (text) {
+      this.element.hidden = false;
+      this.element.innerText = text;
+    } else {
+      this.element.hidden = true;
+    }
+
+    if (color) {
+      this.element.style.color = color;
+    }
+
+    return this.element;
+  }
+
+  public render(root: HTMLElement) {
+    root.appendChild(this.renderText(this.text, this.color));
+  }
+
+  public update(text?: string, color?: string) {
+    this.text = text;
+    this.color = color;
+
+    this.renderText(text, color);
+  }
+}
