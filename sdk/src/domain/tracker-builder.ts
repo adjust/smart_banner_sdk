93ba29f5877cbcdb6d80e58589a6a867f6afe962
Yara Matkova <yara.matkova@adjust.com>
2023-10-09 14:22:01 +0200

    fix: use of a custom deeplink path

--- sdk/src/domain/tracker-builder.ts
+++ sdk/src/domain/tracker-builder.ts
@@ -1,19 +1,17 @@
 import { Context, DeeplinkData } from '../data/types';
-import { Logger } from '../utils/logger';
 import { parseGetParams } from '../utils/parse-get-params';
 import { interpolate } from '../utils/template-interpolaion';
-import { DeviceOS } from '../utils/detect-os';
 
 export interface TrackerData {
   template: string;
   context: Context;
 }
 
-export function buildSmartBannerUrl(os: DeviceOS, data: TrackerData, pageUrl: string, customDeeplinkData: DeeplinkData) {
+export function buildSmartBannerUrl(data: TrackerData, pageUrl: string, customDeeplinkData: DeeplinkData) {
   const template = data.template;
   const customTrackerData = customDeeplinkData || {};
 
-  const deeplink = buildDeeplink(os, data, pageUrl, customTrackerData);
+  const deeplink = buildDeeplink(data, pageUrl, customTrackerData);
 
   const context: Record<string, string> = {
     ...data.context,
@@ -24,13 +22,8 @@ export function buildSmartBannerUrl(os: DeviceOS, data: TrackerData, pageUrl: st
   return interpolate(template, context);
 }
 
-function buildDeeplink(os: DeviceOS, data: TrackerData, pageUrl: string, customDeeplinkData: DeeplinkData): Record<string, string> {
+function buildDeeplink(data: TrackerData, pageUrl: string, customDeeplinkData: DeeplinkData): Record<string, string> {
   let deeplinkTemplate = data.context.deepLinkPath || data.context.deepLink || '';
-  if (os === DeviceOS.Android) {
-    deeplinkTemplate = customDeeplinkData.androidDeepLinkPath || deeplinkTemplate;
-  } else if (os === DeviceOS.iOS) {
-    deeplinkTemplate = customDeeplinkData.iosDeepLinkPath || deeplinkTemplate;
-  }
 
   const context: Record<string, string> = {
     ...data.context,
@@ -38,11 +31,17 @@ function buildDeeplink(os: DeviceOS, data: TrackerData, pageUrl: string, customD
     ...encodeContext(customDeeplinkData.context)
   };
 
+  // The first iteration, interpolates a template received from the BE,
+  // i.e. "{androidAppScheme}://{androidDeepLinkPath}" => "schema://some/path/{screen}"
   deeplinkTemplate = interpolate(deeplinkTemplate, context);
 
+  // The second iteration, replaces placeholders in the deeplink path template
+  // i.e. "schema://some/path/{screen}" => "schema://some/path/promo"
+  const deeplink = interpolate(deeplinkTemplate, context);
+
   return {
-    'deep_link_path': interpolate(deeplinkTemplate, context), // for ios template
-    'deep_link': encodeURIComponent(interpolate(deeplinkTemplate || '', context)) // for android template
+    'deep_link_path': deeplink, // for ios
+    'deep_link': encodeURIComponent(deeplink) // for android
   };
 }
 
