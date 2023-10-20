9492c31ed0b6f841c6ce0c68269312c1c585db6b
Yara Matkova <yara.matkova@adjust.com>
2023-10-20 10:36:13 +0200

    fix: get rid of unwanted transform of keys to be in lower case

--- sdk/src/utils/snake-to-camel-case.ts
+++ sdk/src/utils/snake-to-camel-case.ts
@@ -14,7 +14,7 @@ export type SnakeCaseKeysToCamelCase<T extends object> = {
  * @returns 
  */
 export function stringToCamelCase(text: string): string {
-  const a = text.toLowerCase().replace(/[-_]+(.)?/g, (_, c) => c ? c.toUpperCase() : '');
+  const a = text.replace(/[-_]+(.)?/g, (_, c) => c ? c.toUpperCase() : '');
   return a.substring(0, 1).toLowerCase() + a.substring(1);
 }
 
