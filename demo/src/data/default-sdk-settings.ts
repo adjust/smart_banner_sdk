d4e070669fcf7ae7a2a3d69451bd1b41b95e1daa
Yara Matkova <yara.matkova@adjust.com>
2023-10-24 23:06:50 +0200

    demo: update default init params

--- demo/src/data/default-sdk-settings.ts
+++ demo/src/data/default-sdk-settings.ts
@@ -3,7 +3,7 @@ import { InitialisationOptions } from '@adjustcom/smart-banner-sdk';
 export const defaultSdkSettings: InitialisationOptions = {
   appToken: '6uzo4j2d8hz4',
   logLevel: 'verbose',
-  androidAppSchema: 'myapp',
-  deepLinkPath: 'catalogue/product={product}',
-  context: { promo: 'kitty', product: 't-shirt' }
+  iosDeepLinkPath: 'items/{product}',
+  androidDeepLinkPath: '{path}',
+  context: { promo: 'kitty', product: 't-shirt', path: 'android/{promo}-promo' }
 };
