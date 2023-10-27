356bfa71724b28686b8b3b06ef6b7f7a6bccb15d
Yara Matkova <yara.matkova@adjust.com>
2023-10-27 14:30:30 +0200

    refac: return not found placeholders from interpolation function

--- sdk/src/utils/template-interpolaion.ts
+++ sdk/src/utils/template-interpolaion.ts
@@ -1,25 +1,38 @@
 import { Logger } from './logger';
+
+export interface InterpolationResult {
+  result: string;
+  notReplaced: string[];
+}
+
 /**
  * Replaces in template all occurences of parameters in curly brackets by values founded by names in context.
  * Logs a warning if there is no value for a certain placeholder.
  * 
  * @param template a string containing parameter names in curly brackets
  * @param context an object containing parameters for interpolation
- * @returns interpolated template
+ * @returns an object containing interpolated template as a result and a list of placeholders which were not replaced
  * 
  * @example
  * const greet = interpolate("Hello, {username}!", { username: 'John Smith' });
  * console.log(greet); // prints "Hello John Smith!"
  */
-export function interpolate(template: string, context: Record<string, Primitive>) {
+export function interpolate(template: string, context: Record<string, Primitive>): InterpolationResult {
+  const notReplacedPlaceholders: string[] = [];
+
   const re = /{(\w+)}/g;
   const replacer = (_: string, paramName: string): string => {
     if (context[paramName] === undefined) {
+      notReplacedPlaceholders.push(paramName);
       Logger.warn(`No value for placeholder: {${paramName}}`);
     }
     return String(context[paramName] || '');
   };
-  return template.replace(re, replacer);
+
+  return {
+    result: template.replace(re, replacer),
+    notReplaced: notReplacedPlaceholders
+  };
 }
 
 type Primitive = number | string | boolean | null | undefined
