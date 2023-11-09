0bb9802f7e9ec07f2538234db3f1fa646d67c18c
Yara Matkova <yara.matkova@adjust.com>
2023-11-09 16:45:22 +0100

    fix: `none` logging level value

--- sdk/src/utils/logger.ts
+++ sdk/src/utils/logger.ts
@@ -3,7 +3,7 @@ export type LogLevel = 'none' | 'verbose' | 'info' | 'warning' | 'error'
 
 export class Logger {
   private static LEVELS: Record<LogLevel, number> = {
-    'none': -1,
+    'none': 100,
     'verbose': 1,
     'info': 2,
     'warning': 3,
@@ -18,18 +18,18 @@ export class Logger {
     }
 
     switch (messageLevel) {
-    case 'verbose':
-      console.log(message);
-      break;
-    case 'info':
-      console.log(message);
-      break;
-    case 'warning':
-      console.warn(message);
-      break;
-    case 'error':
-      console.error(message);
-      break;
+      case 'verbose':
+        console.log(message);
+        break;
+      case 'info':
+        console.log(message);
+        break;
+      case 'warning':
+        console.warn(message);
+        break;
+      case 'error':
+        console.error(message);
+        break;
     }
   }
 
