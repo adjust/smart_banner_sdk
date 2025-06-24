34dbd7048a72cfe4f17c52f35e7b3bb6ffad708f
Yara Matkova <yara.matkova@adjust.com>
2025-06-24 13:50:30 +0200

    feat: add utility to select zh language properly

--- sdk/src/utils/language.ts
+++ sdk/src/utils/language.ts
@@ -1,8 +1,22 @@
+import { Localization } from "src/data/types";
+
+// Initial implementation didn't distinguish these languages, we had only one 'zh'.
+// In theory we can get all 3 'zh, 'zh-hant' and 'zh-hans' from the backend. 
+const chineseLanguages = ['zh-hans', 'zh-hant'];
+
 export function getLanguage(): string | null {
   let languageTag = null;
 
   if (navigator) {
-    languageTag = ((Array.isArray(navigator.languages) && navigator.languages.length > 0) ? navigator.languages[0] : navigator.language || '').split('-')[0];
+    let language = ((Array.isArray(navigator.languages) && navigator.languages.length > 0) ? navigator.languages[0] : navigator.language) || '';
+    language = language.toLowerCase();
+
+    if (chineseLanguages.includes(language)) {
+      // return 'zh-hant' or 'zh-hans' as is
+      return language;
+    }
+
+    languageTag = language.split('-')[0];
 
     if (languageTag.length < 2) {
       languageTag = null;
@@ -10,4 +24,33 @@ export function getLanguage(): string | null {
   }
 
   return languageTag || null;
+}
+
+/**
+ * Check if data contains detected Chinese language, otherwise try to fallback.
+ */
+export function getCompatibleZhLanguage(localizations: { [key: string]: Localization }, lang: string | null): string | null {
+  if (!lang || !['zh', ...chineseLanguages].includes(lang)) {
+    return lang; // It's not Chinese at all
+  }
+
+  if (localizations[lang]) {
+    return lang; // Localization is present, nothing to do here
+  }
+
+  if (chineseLanguages.includes(lang) && localizations['zh']) {
+    // We detect one of 'modern' Chinese locale, but there is no such key in localizations data
+    return 'zh'; // Fallback to 'outdated' one
+  }
+
+  if (lang === 'zh') {
+    // We detect zh locale, and localizations data does not contain it
+    return localizations[chineseLanguages[0]] ?
+      chineseLanguages[0] : // return 'zh-hans' if it's present
+      localizations[chineseLanguages[1]]
+        ? chineseLanguages[1]  // return 'zh-hant' otherwise
+        : null
+  }
+
+  return null;
 }
--- test/utils/language.spec.ts
+++ test/utils/language.spec.ts
@@ -1,4 +1,5 @@
-import { getLanguage } from '@sdk/utils/language';
+import { Localization } from '@sdk/data/types';
+import { getLanguage, getCompatibleZhLanguage } from '@sdk/utils/language';
 
 describe('Detecting browser language', () => {
   afterAll(() => {
@@ -35,3 +36,58 @@ describe('Detecting browser language', () => {
     expect(getLanguage()).toBe(null);
   });
 });
+
+describe('Using proper zh language from data', () => {
+  const en = { title: 'en title', description: 'English' } as Localization
+  const zh = { title: 'zh title', description: 'Deprecated Chinese locale' } as Localization
+  const zhHans = { title: 'zh-hans title', description: 'Simplified Chinese' } as Localization
+  const zhHant = { title: 'zh-hant title', description: 'Traditional Chinese' } as Localization
+
+  const localization = { en, zh, 'zh-hans': zhHans, 'zh-hant': zhHant }
+
+  test.each([
+    'en',
+    null,
+    'de'
+  ])('returns original value when detected language is not one of zh, zh-hant, zh-hans', (lang) => {
+    expect(getCompatibleZhLanguage(localization, lang)).toBe(lang)
+  });
+
+  test.each([
+    'zh',
+    'zh-hans',
+    'zh-hant'
+  ])('returns original value when detected language is one of zh, zh-hant, zh-hans and such localization is present', (lang) => {
+    expect(getCompatibleZhLanguage(localization, lang)).toBe(lang)
+  });
+
+  test.each([
+    'zh-hans',
+    'zh-hant'
+  ])('returns zh when modern locale is detected but data contains only zh localization data', (lang) => {
+    expect(getCompatibleZhLanguage({ en, zh }, lang)).toBe('zh')
+  });
+
+  test.each([
+    'zh',
+    'zh-hans',
+    'zh-hant'
+  ])('returns null when detected language is not present in localization data', (lang) => {
+    expect(getCompatibleZhLanguage({ en }, lang)).toBe(null)
+  });
+
+  describe('detected browser language is zh', () => {
+    it('priorities zh-hans', () => {
+      expect(getCompatibleZhLanguage({ en, 'zh-hans': zhHans, 'zh-hant': zhHant }, 'zh')).toBe('zh-hans')
+    })
+
+    it('returns zh-hant if there is no zh-hans in localization data', () => {
+      expect(getCompatibleZhLanguage({ en, 'zh-hant': zhHant }, 'zh')).toBe('zh-hant')
+    })
+
+    it('returns null if no appropriate localization found', () => {
+      expect(getCompatibleZhLanguage({ en }, 'zh')).toBe(null)
+    })
+  })
+
+})
